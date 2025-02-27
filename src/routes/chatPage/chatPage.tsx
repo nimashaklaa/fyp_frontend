4acba1089d441eb46d08f31149709365d315d7ab
nimashaklaa <nimashaklaa.20@cse.mrt.ac.lk>
2025-02-27 15:34:13 +0530

    feat: chat page styling

--- src/routes/chatPage/chatPage.tsx
+++ src/routes/chatPage/chatPage.tsx
@@ -1,9 +1,39 @@
 import './chatPage.css'
-import React from "react";
+import React, {useEffect, useRef} from "react";
 
 const ChatPage: React.FC =()=>{
+
+    // this is used to automatically scroll down to the end of the chat
+    const endRef =useRef<HTMLDivElement | null>(null)
+
+    useEffect(() => {
+        endRef.current?.scrollIntoView({behavior:"smooth"})
+    }, []);
+
     return(
-        <div className='chatpage'>ChatPage</div>
+        <div className='chatPage'>
+            <div className="wrapper">
+                <div className="chat">
+                    <div className="message">Test msg from ai</div>
+                    <div className="message user">Test msg from user</div>
+                    <div className="message">Test msg from ai</div>
+                    <div className="message user">Test msg from user</div>
+                    <div className="message">Test msg from ai</div>
+                    <div className="message user">Test msg from user</div>
+                    <div className="message">Test msg from ai</div>
+                    <div className="message user">Test msg from user</div>
+                    <div className="message">Test msg from ai</div>
+                    <div className="message user">Test msg from user</div>
+                    <div className="message">Test msg from ai</div>
+                    <div className="message user">Test msg from user</div>
+                    <div className="message">Test msg from ai</div>
+                    <div className="message user">Test msg from user</div>
+                    <div className="message">Test msg from ai</div>
+                    <div className="message user">Test msg from user</div>
+                    <div ref={endRef}/>
+                </div>
+            </div>
+        </div>
     )
 }
 export default ChatPage;
